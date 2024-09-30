bf29741c0cff62ee25e4b3f8d03fe132b087ea44
JanaSalem <jana.salem@edu.esiee.fr>
2024-09-30 12:40:49 +0000

    Voici le code, exercice sur les Tuples de Salem Jana

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-#### Imports et définition des variables globales
-
-
-#### Fonctions secondaires
+"""Voici le Code de Salem Jana"""
+import sys
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne
+      de caractères passée en argument selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -13,14 +12,29 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
+    compteur = 1
+    count = []
 
-    return [ ]
+    for i in range(len(s)-1): # on met -1 pour pas causer un index hors limites
 
+        if s[i] == s[i+1]:
+            compteur = compteur + 1
+
+        else:
+            count.append((s[i], compteur))
+            compteur = 1
+
+    count.append((s[-1], compteur))  # ne pas oublier d'ajouter le dernier caractère
+
+    return count
+
+
+
+sys.setrecursionlimit(1200)  # Car erreru de recursion
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne de 
+    caractères passée en argument selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -28,20 +42,22 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
+    if len(s) == 0:  # Cas de base
+        return []
 
-    # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
+    compteur = 1
+    while compteur < len(s) and s[compteur] == s[0]:
+        compteur += 1
+    tuplle = (s[0], compteur) # Créer le tuple pour le premier caractère
 
-    return []
-    
+    return [tuplle] + artcode_r(s[compteur:])  # Appel récursif
 
 #### Fonction principale
 
 
 def main():
+    """La Fonction main
+    """
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
